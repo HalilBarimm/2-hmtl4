8050aed2093f3be4a13d48f0790c62bdcb0d8c04
HalilBarimm <halilbarim@gmail.com>
2015-11-05 14:44:58 +0300

    html4 formlari

---
+++ messageSent.php
@@ -0,0 +1,27 @@
+<html>
+<meta charset="utf-8">
+<head>
+	<title>Yazı Gönder</title>
+	<link rel="stylesheet" type="text/css" href="styleCss.css">
+</head>
+<body>
+<div id="container">
+		<form name="messageSent" id="messageSentForm" method="post">
+			<table cols="6" rows="6">
+				<tr>
+					<td><label>Başlık:</label></td><td><input type="text" name="title" id="titleİnput"></td>
+				</tr>
+				<tr>
+					<td><label>Metin:</label></td><td><input type="text" name="text" id="textİnput"></td>
+				</tr>
+				<tr>
+					<td></td><td><input type="button" id="messageSentButton" value="Gönder"></td>
+				</tr>
+			</table>
+
+		</form>
+
+
+</div>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,29 @@
+<html>
+<meta charset="utf-8">
+<head>
+	<title>Kayıt Ol</title>
+</head>
+<body>
+	<div id="contanier">
+		<form name="register" id="registerForm" method="post">
+			<table>
+				<tr>
+					<td><label>Kullanıcı Adı:</label></td><td><input type="text" name="userName" id="userNameİnput"></td>
+				</tr>
+				<tr>
+					<td><label>Kullanıcı Şifre:</label></td><td><input type="text" name="userPass" id="userPassİnput"></td>
+				</tr>
+				<tr>
+					<td><label>Email:</label></td><td><input type="text" name="userEmail" id="userEmailİnput"></td>
+				</tr>
+				<tr>
+					<td></td><td><input type="button" id="userRegisterButton" value="Kayıt Ol"></td>
+				</tr>
+			</table>
+
+		</form>
+
+
+	</div>
+</body>
+</html>
